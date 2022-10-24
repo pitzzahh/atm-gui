b2766c82ed14372e6fc78d060330b754d24fff4e
pitzzahh <araopj2191@gmail.com>
2022-10-24 21:24:30 +0800

    renamed single method from checkAccountNumber to doesAccountExist

--- src/main/java/io/github/pitzzahh/atm/validator/Validator.java
+++ src/main/java/io/github/pitzzahh/atm/validator/Validator.java
@@ -6,7 +6,7 @@ import static io.github.pitzzahh.util.utilities.validation.Validator.isWholeNumb
 
 public final class Validator {
 
-    public static boolean checkAccountNumber(String accountNumber) {
+    public static boolean doesAccountExist(String accountNumber) {
         if (accountNumber.length() != 9) throw new IllegalArgumentException("Account number must be 9 digits long");
         if (isWholeNumber().negate().test(accountNumber)) throw new IllegalArgumentException("Account number must be a number");
         return Atm.getService().getAllClients().get()
