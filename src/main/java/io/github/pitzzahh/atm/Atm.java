f7f1d48717451f95eb959f04cc71e3b53e81f49a
pitzzahh <araopj2191@gmail.com>
2022-10-24 21:18:23 +0800

    refactor to InMemory database and added a single client

--- src/main/java/io/github/pitzzahh/atm/Atm.java
+++ src/main/java/io/github/pitzzahh/atm/Atm.java
@@ -1,21 +1,20 @@
 package io.github.pitzzahh.atm;
 
-import io.github.pitzzahh.atm.database.DatabaseConnection;
-import io.github.pitzzahh.atm.entity.Client;
-import io.github.pitzzahh.atm.service.AtmService;
-import io.github.pitzzahh.atm.dao.InDatabase;
-import io.github.pitzzahh.util.utilities.Print;
-import io.github.pitzzahh.util.utilities.classes.Person;
 import io.github.pitzzahh.util.utilities.classes.enums.Gender;
+import io.github.pitzzahh.util.utilities.classes.Person;
+import io.github.pitzzahh.atm.service.AtmService;
+import io.github.pitzzahh.atm.entity.Client;
+import io.github.pitzzahh.atm.dao.InMemory;
 import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.StageStyle;
 import javafx.fxml.FXMLLoader;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.Month;
-
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import java.util.Objects;
+import java.time.Month;
 
 public class Atm extends Application {
 
@@ -23,29 +22,31 @@ public class Atm extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        var fxmlLoader = new FXMLLoader(Atm.class.getResource("hello-view.fxml"));
+        var fxmlLoader = new FXMLLoader(Atm.class.getResource("login_page.fxml"));
         var scene = new Scene(fxmlLoader.load());
         stage.initStyle(StageStyle.UNDECORATED);
+        stage.getIcons().add(new Image(Objects.requireNonNull(Atm.class.getResourceAsStream("img/logo.png"), "logo not found")));
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        service = new AtmService(new InDatabase());
-        service.setDataSource()
-                        .accept(
-                                new DatabaseConnection()
-                                        .setDriverClassName("org.postgresql.Driver")
-                                        .setUrl("jdbc:postgresql://localhost:5432/postgres")
-                                        .setUsername("postgres")
-                                        .setPassword("!P4ssW0rd@123")
-                                        .getDataSource()
-                        );
-
-        service.getAllClients()
-                .get()
-                .values()
-                .forEach(Print::println);
+        service = new AtmService(new InMemory());
+        service.saveClient().apply(
+                new Client(
+                        "123123123",
+                        "123123",
+                        new Person(
+                                "Mark",
+                                "Silent",
+                                Gender.PREFER_NOT_TO_SAY,
+                                "Earth",
+                                LocalDate.of(2018, Month.AUGUST, 10)
+                        ),
+                        5555,
+                        false
+                )
+        );
         launch();
     }
 
