53c14d897b4b030a639a55ae8e9a4d24cb3d9804
pitzzahh <araopj2191@gmail.com>
2022-10-25 07:22:00 +0800

    added more condition on checking accounts

--- src/main/java/io/github/pitzzahh/atm/validator/Validator.java
+++ src/main/java/io/github/pitzzahh/atm/validator/Validator.java
@@ -7,8 +7,9 @@ import static io.github.pitzzahh.util.utilities.validation.Validator.isWholeNumb
 public final class Validator {
 
     public static boolean doesAccountExist(String accountNumber) {
-        if (accountNumber.length() != 9) throw new IllegalArgumentException("Account number must be 9 digits long");
-        if (isWholeNumber().negate().test(accountNumber)) throw new IllegalArgumentException("Account number must be a number");
+        if (accountNumber.isEmpty()) throw new RuntimeException("Please enter your account number");
+        else if (accountNumber.length() != 9) throw new IllegalArgumentException("Account number must be 9 digits long");
+        else if (isWholeNumber().negate().test(accountNumber)) throw new IllegalArgumentException("Account number must be a number");
         return Atm.getService().getAllClients().get()
                 .values()
                 .stream()
