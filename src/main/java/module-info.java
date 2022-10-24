a661df9477ad9733bc72f0bdc0c38b3785171919
pitzzahh <araopj2191@gmail.com>
2022-10-24 22:19:34 +0800

    added requires for logging modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module io.github.pitzzahh.atm {
     requires util.classes;
     requires automated.teller.machine.API;
     requires java.sql;
+    requires ch.qos.logback.core;
+    requires org.slf4j;
 
     opens io.github.pitzzahh.atm.controllers to javafx.fxml;
     exports io.github.pitzzahh.atm;
