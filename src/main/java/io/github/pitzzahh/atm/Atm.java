145e11fb7638e0fe813e709da99be215b2cee25b
pitzzahh <araopj2191@gmail.com>
2022-10-24 22:35:04 +0800

    added logic for dragging window

--- src/main/java/io/github/pitzzahh/atm/Atm.java
+++ src/main/java/io/github/pitzzahh/atm/Atm.java
@@ -8,26 +8,44 @@ import io.github.pitzzahh.atm.dao.InMemory;
 import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.StageStyle;
+import org.slf4j.LoggerFactory;
 import javafx.fxml.FXMLLoader;
 import java.io.IOException;
 import java.time.LocalDate;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.util.Objects;
+import org.slf4j.Logger;
 import java.time.Month;
 
 public class Atm extends Application {
 
     private static AtmService service;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Atm.class);
+
+    private double x = 0;
+    private double y = 0;
 
     @Override
     public void start(Stage stage) throws IOException {
-        var fxmlLoader = new FXMLLoader(Atm.class.getResource("login_page.fxml"));
-        var scene = new Scene(fxmlLoader.load());
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Atm.class.getResource("login_page.fxml")));
+        var scene = new Scene(root);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.getIcons().add(new Image(Objects.requireNonNull(Atm.class.getResourceAsStream("img/logo.png"), "logo not found")));
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+
         stage.setScene(scene);
         stage.show();
+        LOGGER.info("Application started");
     }
 
     public static void main(String[] args) {
@@ -52,5 +70,8 @@ public class Atm extends Application {
 
     public static AtmService getService() {
         return service;
+    }
+    public static Logger getLogger() {
+        return LOGGER;
     }
 }
