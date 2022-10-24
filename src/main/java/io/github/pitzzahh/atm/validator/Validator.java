a0e67b6a8400f82c7ab2d9c722cedfd6d9cfeea5
pitzzahh <araopj2191@gmail.com>
2022-10-25 07:41:57 +0800

    added logging

--- src/main/java/io/github/pitzzahh/atm/validator/Validator.java
+++ src/main/java/io/github/pitzzahh/atm/validator/Validator.java
@@ -1,8 +1,10 @@
 package io.github.pitzzahh.atm.validator;
 
-import io.github.pitzzahh.atm.Atm;
-import io.github.pitzzahh.atm.entity.Client;
 import static io.github.pitzzahh.util.utilities.validation.Validator.isWholeNumber;
+import static io.github.pitzzahh.atm.Atm.getLogger;
+import io.github.pitzzahh.atm.entity.Client;
+import static java.lang.String.format;
+import io.github.pitzzahh.atm.Atm;
 
 public final class Validator {
 
@@ -10,11 +12,13 @@ public final class Validator {
         if (accountNumber.isEmpty()) throw new RuntimeException("Please enter your account number");
         else if (accountNumber.length() != 9) throw new IllegalArgumentException("Account number must be 9 digits long");
         else if (isWholeNumber().negate().test(accountNumber)) throw new IllegalArgumentException("Account number must be a number");
-        return Atm.getService().getAllClients().get()
+        var doesExist = Atm.getService().getAllClients().get()
                 .values()
                 .stream()
                 .map(Client::accountNumber)
                 .anyMatch(accountNumber::equals);
+        getLogger().debug(format("Does account exist %s", doesExist));
+        return doesExist;
     }
 
 }
