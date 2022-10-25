e23b5375377938dcb6cd4e34281a96322b8327a4
pitzzahh <araopj2191@gmail.com>
2022-10-25 13:56:24 +0800

    added moveWindow util method

--- src/main/java/io/github/pitzzahh/atm/util/Util.java
+++ src/main/java/io/github/pitzzahh/atm/util/Util.java
@@ -1,17 +1,34 @@
 package io.github.pitzzahh.atm.util;
 
+import java.util.concurrent.atomic.AtomicReference;
 import javafx.animation.PauseTransition;
+import io.github.pitzzahh.atm.Atm;
 import javafx.scene.control.Label;
+import javafx.scene.Parent;
 import javafx.util.Duration;
 
-public class Util {
+public interface Util {
 
-    public static void remove(Label label, Duration duration) {
+    AtomicReference<Double> horizontal = new AtomicReference<>(0.0);
+    AtomicReference<Double> vertical = new AtomicReference<>(0.0);
+
+    static void remove(Label label, Duration duration) {
         var visiblePause = new PauseTransition(duration);
         visiblePause.setOnFinished(
                 event -> label.setVisible(false)
         );
         visiblePause.play();
+    }
+
+    static void moveWindow(Parent parent) {
+        parent.setOnMousePressed(event -> {
+            horizontal.set(event.getSceneX());
+            vertical.set(event.getSceneY());
+        });
+        parent.setOnMouseDragged(event -> {
+            Atm.getStage().setX(event.getScreenX() - horizontal.get());
+            Atm.getStage().setY(event.getScreenY() - vertical.get());
+        });
     }
 
 }
