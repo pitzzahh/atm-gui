8ffcba23eafdad19e3bcf7f7b8cb2276925a32d4
pitzzahh <araopj2191@gmail.com>
2022-10-25 14:18:28 +0800

    added parent adding

--- src/main/java/io/github/pitzzahh/atm/util/Util.java
+++ src/main/java/io/github/pitzzahh/atm/util/Util.java
@@ -1,5 +1,8 @@
 package io.github.pitzzahh.atm.util;
 
+import io.github.pitzzahh.util.utilities.classes.DynamicArray;
+
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
 import javafx.animation.PauseTransition;
 import io.github.pitzzahh.atm.Atm;
@@ -11,6 +14,7 @@ public interface Util {
 
     AtomicReference<Double> horizontal = new AtomicReference<>(0.0);
     AtomicReference<Double> vertical = new AtomicReference<>(0.0);
+    DynamicArray<Parent> parents = new DynamicArray<>();
 
     static void remove(Label label, Duration duration) {
         var visiblePause = new PauseTransition(duration);
@@ -29,6 +33,21 @@ public interface Util {
             Atm.getStage().setX(event.getScreenX() - horizontal.get());
             Atm.getStage().setY(event.getScreenY() - vertical.get());
         });
+    }
+
+    static void addParent(Parent parent) {
+        parents.insert(parent);
+    }
+
+    static void addParents(Parent... p) {
+        Arrays.stream(p).forEach(parents::insert);
+    }
+
+    static Parent getWindow(String name) {
+        return parents.stream()
+                .filter(parent -> parent.getId().equals(name))
+                .findAny()
+                .orElse(parents.get(0));
     }
 
 }
