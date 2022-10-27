e68fa109f53cadf0351b88c6d2cdc95d8454ba3d
pitzzahh <pitzzahh@gmail.com>
2022-10-27 13:40:18 +0800

    Added tooltips for admin window button

--- src/main/java/io/github/pitzzahh/atm/controllers/AdminController.java
+++ src/main/java/io/github/pitzzahh/atm/controllers/AdminController.java
@@ -1,7 +1,50 @@
 package io.github.pitzzahh.atm.controllers;
 
+import static io.github.pitzzahh.atm.util.Util.adminButtonFunctionsToolTip;
+import static io.github.pitzzahh.atm.util.Util.initToolTip;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
 import javafx.fxml.FXML;
 
 public class AdminController {
 
+    @FXML
+    private Button addClients;
+
+    @FXML
+    private Button removeClients;
+
+    @FXML
+    private Button viewClients;
+
+    public void onMouseEnteredAddClientsButton(MouseEvent mouseEvent) {
+        var tooltip = initToolTip(
+                "Add Clients",
+                mouseEvent,
+                adminButtonFunctionsToolTip()
+                );
+        tooltip.setShowDuration(Duration.seconds(3));
+        addClients.setTooltip(tooltip);
+    }
+
+    public void onMouseEnteredRemoveClientsButton(MouseEvent mouseEvent) {
+        var tooltip = initToolTip(
+                "Remove Clients",
+                mouseEvent,
+                adminButtonFunctionsToolTip()
+        );
+        tooltip.setShowDuration(Duration.seconds(3));
+        removeClients.setTooltip(tooltip);
+    }
+
+    public void onMouseEnteredViewClientsButton(MouseEvent mouseEvent) {
+        var tooltip = initToolTip(
+                "View the list of Clients information",
+                mouseEvent,
+                adminButtonFunctionsToolTip()
+        );
+        tooltip.setShowDuration(Duration.seconds(3));
+        viewClients.setTooltip(tooltip);
+    }
 }
