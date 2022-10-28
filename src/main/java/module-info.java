d4d526e4cf5893991e95218900a75967322e83f8
pitzzahh <pitzzahh@gmail.com>
2022-10-28 23:05:00 +0800

    fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,11 @@
-module io.github.pitzzahh.atm {
+module io.github.pitzzahh.atm.gui {
     requires javafx.controls;
     requires javafx.fxml;
     requires util.classes;
     requires automated.teller.machine.API;
-    requires java.sql;
     requires ch.qos.logback.core;
     requires org.slf4j;
 
-    opens io.github.pitzzahh.atm.controllers to javafx.fxml;
-    exports io.github.pitzzahh.atm;
+    opens io.github.pitzzahh.atmGui.controllers to javafx.fxml;
+    exports io.github.pitzzahh.atmGui;
 }
